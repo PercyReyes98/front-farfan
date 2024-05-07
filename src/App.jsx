b6c3273c57a7bfccfbffcfd1d53253589582c858
Percy Reyes <alfredocancer98@gmail.com>
2024-05-07 12:24:06 -0500

    "Custom hook declared remove"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Report from './components/Report'
-import {UseReport} from './components/hooks/useReport'
+
 //import './App.css'
 
 function App() {
-  const {report} = UseReport()
-  console.log(report)
   //const {exercise, period}= report
   return (
     <>
-      <Report report={report}  ></Report>
+      <Report ></Report>
     </>
   )
 }
--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {UseReport} from './hooks/useReport'
 /*
             <p>Compañia: {report.company.name}</p>
             <p>Ejercicio: {report.exercise}</p>
@@ -29,14 +30,42 @@ import React from "react";
             <p>Impuesto a la Renta: {report.taxes.monthly_rent}</p>
             <p>Total: {report.taxes.total}</p> 
 */
-const Report = ({report})=>{
-    
+const Report = ()=>{
+    const {report} = UseReport()
+    console.log(report)
     return (   
         <>
+            {report.length !== 0 ?(
+                <>
+                <p>Compañia: {report.company.name}</p>
             <p>Ejercicio: {report.exercise}</p>
             <p>Periodo: {report.period} </p>
-            
+            <p>Ingresos</p>
+            <p>--------------------</p>
+            <p>Banco: {report.icome[0].financeCompany.name}</p>
+            <p>Monto: {report.icome[0].amount}</p>
+            <p>Banco: {report.icome[1].financeCompany.name}</p>
+            <p>Monto: {report.icome[1].amount}</p>
+            <p>Banco: {report.icome[2].financeCompany.name}</p>
+            <p>Monto: {report.icome[2].amount}</p>
+            <p>Proyecciones</p>
+            <p>---------------------</p>
+            <p>Ventas</p>
+            <p>Monto: {report.projections.sales.worth}</p>
+            <p>IGV: {report.projections.sales.igv}</p>
+            <p>Total: {report.projections.sales.total}</p>
+            <p>----------------------</p>
+            <p>Compras</p>
+            <p>Monto: {report.projections.shopping.worth}</p>
+            <p>IGV: {report.projections.shopping.igv}</p>
+            <p>Total: {report.projections.shopping.total}</p>
+            <p>Impuestos</p>
+            <p>IGV del periodo: {report.taxes.of_the_period}</p>
+            <p>Creditos a favor anterior: {report.taxes.credit_in_favor}</p>
+            <p>IGV por devolver: {report.taxes.to_return}</p>
+            <p>Impuesto a la Renta: {report.taxes.monthly_rent}</p>
+            <p>Total: {report.taxes.total}</p>
+                </>): null }
         </>
-    )
-}
+    )}
 export default Report
