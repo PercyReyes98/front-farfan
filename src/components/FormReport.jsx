83fd0810581d03e7d1dfad52b41e073a783b273a
Percy Reyes <alfredocancer98@gmail.com>
2024-05-13 12:40:54 -0500

    "Use Efect added"

--- src/components/FormReport.jsx
+++ src/components/FormReport.jsx
@@ -1,103 +1,55 @@
-import { useState} from "react"
+import { useEffect, useState} from "react"
 const FormReport = ()=>{
-    const [ company, setCompany] = useState({
-        ruc:'',
-        name:''
-    })
-    const [bcp, setBcp] = useState({
-        name: "BCP",
-        ruc: "20553321488",
-        amount: null
-    })
-    const [interbank, setInterbank] = useState({
-        name: "Interbank",
-        ruc: "20558869123",
-        amount: null
-    })
-    const [bbva, setBbva] = useState({
-        name: "BBVA",
-        ruc: "2085789632",
-        amount: null
-    })
-    const [sales, setSales] = useState({
-        total: null,
-        worth:null,
-        igv: null
-    })
-    const [shopping, setShopping] = useState({
-        total: null,
-        worth:null,
-        igv: null
-    })
-    const [taxes, setTaxes] = useState({
-        of_the_period:null,
-        credit_in_favor:null,
-        to_return:null,
-        monthly_rent:null,
-        total:null
-    })
-    
-    const [rang, setRang] = useState({
-        period: '',
-        exercise:''
-    })
-    const handleChangeCompany = (event)=>{
-        const {name,value} = event.target
-        setCompany({
-            ...company,
-            [name]: value
-        })
-    }
-    const handleChangeBCP = (event)=>{
-        const {name,value} = event.target
-        setBcp({
-            ...bcp,
-            [name]: value
-        })
-    }
-    const handleChangeINTERBANK = (event)=>{
-        const {name,value} = event.target
-        setInterbank({
-            ...interbank,
-            [name]: value
-        })
-    }
-    const handleChangeBBVA = (event)=>{
-        const {name,value} = event.target
-        setBbva({
-            ...bbva,
-            [name]: value
-        })
-    }
-    const handleChangeSales = (event)=>{
-        const {name,value} = event.target
-        setSales({
-            ...sales,
-            [name]: value
-        })
-    }
-    const handleChangeShoppping = (event)=>{
-        const {name,value} = event.target
-        setShopping({
-            ...shopping,
-            [name]: value
-        })
-    }
-    const handleChangeTaxes = (event)=>{
-        const {name,value} = event.target
-        setTaxes({
-            ...taxes,
-            [name]: value
-        })
-    } 
+    const [report, setReport] = useState({
+        company_ruc:"",
+        company_name:"",
+        period:"",
+        exercise:"",
+        bcp_amount:0,
+        interbank_amount:0,
+        bbva_amount:0,
+        icome_total:0,
+        projection_sales_worth:0,
+        projection_sales_igv:0,
+        projection_sales_total:0,
+        projection_shopping_worth:0,
+        projection_shopping_igv:0,
+        projection_shopping_total:0,
+        taxes_of_the_period:0,
+        taxes_credit_in_favor:0,
+        taxes_to_return:0,
+        taxes_monthly_rent:0,
+        taxes_total:0
+    }) 
     const handleChange = (event)=>{
         const {name,value} = event.target
-        setRang({
-            ...rang,
+        setReport({
+            ...report,
             [name]: value
         })
     } 
-           
+    useEffect(()=>{
+        if(report.bcp_amount != 0 && report.interbank_amount != 0 && report.bbva_amount != 0){
+            setReport(report => ({
+                ...report,
+                icome_total: Number(report.bcp_amount) + Number(report.interbank_amount) + Number(report.bbva_amount)
+            }))
+        }
+        if(report.projection_sales_total != 0 && report.projection_shopping_total != 0){
+            setReport(report =>({
+                ...report,
+                projection_sales_worth:(report.projection_sales_total != 0) ? (Number(report.projection_sales_total)/1.18).toFixed(2) : 0,
+                projection_sales_igv: (report.projection_sales_worth !=0)? (Number(report.projection_sales_worth)*0.18).toFixed(2) : 0,
+                projection_shopping_worth:(report.projection_shopping_total != 0) ? (Number(report.projection_shopping_total)/1.18).toFixed(2) : 0,
+                projection_shopping_igv:(report.projection_shopping_worth != 0)? (Number(report.projection_shopping_worth)*0.18).toFixed(2) : 0,
+                taxes_of_the_period: (report.projection_sales_igv != 0 && report.projection_shopping_igv !=0) ?Number(report.projection_sales_igv) - Number(report.projection_shopping_igv) : 0,
+                taxes_to_return: (report.taxes_of_the_period != 0)? Math.round(Number(report.taxes_of_the_period)) : 0,
+                taxes_monthly_rent:(report.projection_sales_igv != 0)? (Number(report.projection_sales_worth)*0.01).toFixed(0) : 0,
+                taxes_total: (report.taxes_to_return <= 0) ? report.taxes_monthly_rent : Number(report.taxes_to_return) + Number(report.taxes_monthly_rent)
+
+            }))
+        }
+    }, [report.bcp_amount, report.bbva_amount, report.interbank_amount, report.projection_sales_total, report.projection_shopping_total ])    
         
           const handleSubmit = async (event) => {
             event.preventDefault();
@@ -108,17 +60,7 @@ const FormReport = ()=>{
                 headers: {
                   'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({company,
-                    period: rang.period,
-                    exercise:rang.exercise,
-                    icome:[
-                        {financeCompany:{bcp}},
-                        {financeCompany:{interbank}},
-                        {financeCompany:{bbva}}],
-                    projections:{
-                        sales,
-                        shopping},
-                        taxes})
+                body: JSON.stringify(report)
               });
         
               if (response.ok) {
@@ -138,22 +80,24 @@ const FormReport = ()=>{
             <fieldset>
                 <legend>Empresa</legend>
                 <label>RUC</label>
-                <input type="text" name="ruc"  id="ruc" value={company.ruc} onChange={handleChangeCompany} placeholder="ruc"/><br />
+                <input type="text" name="company_ruc"  id="ruc" value={report.company_ruc} onChange={handleChange} placeholder="ruc"/><br />
                 <label>Razon Social</label>
-                <input type="text" name="name"  id="razonSocial" value={company.name} onChange={handleChangeCompany} placeholder="razonSocial"/><br />
+                <input type="text" name="company_name"  id="razonSocial" value={report.company_name} onChange={handleChange} placeholder="razonSocial"/><br />
                 <label>Periodo</label>
-                <input type="text" name="period"  id="periodo" value={rang.period} onChange={handleChange} placeholder="periodo"/><br />
+                <input type="text" name="period"  id="periodo" value={report.period} onChange={handleChange} placeholder="periodo"/><br />
                 <label>Ejercicio</label>
-                <input type="text" name="exercise"  id="ejercicio" value={rang.exercise} onChange={handleChange} placeholder="ejercicio"/><br />
+                <input type="text" name="exercise"  id="ejercicio" value={report.exercise} onChange={handleChange} placeholder="ejercicio"/><br />
             </fieldset>
             <fieldset>
                 <legend>Ingresos</legend>
                 <label>BCP</label>
-                <input type="number" name="amount"  id="bcp" value={bcp.amount} onChange={handleChangeBCP} placeholder="monto"/><br />
+                <input type="number" name="bcp_amount"  id="bcp" value={report.bcp_amount} onChange={handleChange} placeholder="monto"/><br />
                 <label>Interbak</label>
-                <input type="number" name="amount"  id="interbank" value={interbank.amount} onChange={handleChangeINTERBANK} placeholder="monto"/><br />
+                <input type="number" name="interbank_amount"  id="interbank" value={report.interbank_amount} onChange={handleChange} placeholder="monto"/><br />
                 <label>BBVA</label>
-                <input type="number" name="amount"  id="bbva" value={bbva.amount} onChange={handleChangeBBVA} placeholder="monto"/><br />
+                <input type="number" name="bbva_amount"  id="bbva" value={report.bbva_amount} onChange={handleChange} placeholder="monto"/><br />
+                <label>Total</label>
+                <input disabled type="number" name="icome_total"  id="bbva" value={report.icome_total} onChange={handleChange} placeholder="monto"/><br />
             </fieldset>
             <table>
                 <caption>
@@ -170,15 +114,15 @@ const FormReport = ()=>{
                 <tbody>
                     <tr>
                     <th scope="row">Proyeccion de ventas</th>
-                    <td><input  type="number" name="worth" id="" value={sales.worth} onChange={handleChangeSales} /></td>
-                    <td><input  type="number" name="igv" id="" value={sales.igv} onChange={handleChangeSales} /></td>
-                    <td><input type="number" name="total" id=""  value={sales.total} onChange={handleChangeSales}/></td>
+                    <td><input  disabled type="number" name="projection_sales_worth" id="" value={report.projection_sales_worth} onChange={handleChange} /></td>
+                    <td><input  disabled type="number" name="projection_sales_igv" id="" value={report.projection_sales_igv} onChange={handleChange} /></td>
+                    <td><input type="number" name="projection_sales_total" id=""  value={report.projection_sales_total} onChange={handleChange}/></td>
                     </tr>
                     <tr>
                     <th scope="row">Compras adquiridas</th>
-                    <td><input  type="number" name="worth" id="" value={shopping.worth} onChange={handleChangeShoppping} /></td>
-                    <td><input  type="number" name="igv" id="" value={shopping.igv} onChange={handleChangeShoppping}/></td>
-                    <td><input type="number" name="total" id="" value={shopping.total} onChange={handleChangeShoppping}/></td>
+                    <td><input disabled type="number" name="projection_shopping_worth" id="" value={report.projection_shopping_worth} onChange={handleChange} /></td>
+                    <td><input disabled type="number" name="projection_shopping_igv" id="" value={report.projection_shopping_igv} onChange={handleChange}/></td>
+                    <td><input type="number" name="projection_shopping_total" id="" value={report.projection_shopping_total} onChange={handleChange}/></td>
                     </tr>
                 </tbody>
                 </table>
@@ -189,23 +133,23 @@ const FormReport = ()=>{
                 <tbody>
                     <tr>
                     <th scope="row">IGV del periodo</th>
-                    <td><input  type="number" name="of_the_period" id="" value={taxes.of_the_period} onChange={handleChangeTaxes}/></td>
+                    <td><input disabled type="number" name="taxes_of_the_period" id="" value={report.taxes_of_the_period} onChange={handleChange}/></td>
                     </tr>
                     <tr>
                     <th scope="row">Credito a favor anterior</th>
-                    <td><input type="number" name="credit_in_favor" id="" value={taxes.credit_in_favor} onChange={handleChangeTaxes}/></td>
+                    <td><input type="number" name="taxes_credit_in_favor" id="" value={report.taxes_credit_in_favor} onChange={handleChange}/></td>
                     </tr>
                     <tr>
                     <th scope="row">IGV por devolver</th>
-                    <td><input  type="number" name="to_return" id="" value={taxes.to_return} onChange={handleChangeTaxes}/></td>
+                    <td><input disabled type="number" name="taxes_to_return" id="" value={report.taxes_to_return} onChange={handleChange}/></td>
                     </tr>
                     <tr>
                     <th scope="row">Impuesto a la renta mensual(1%)</th>
-                    <td><input  type="number" name="monthly_rent" id="" value={taxes.monthly_rent} onChange={handleChangeTaxes} /></td>
+                    <td><input disabled type="number" name="taxes_monthly_rent" id="" value={report.taxes_monthly_rent} onChange={handleChange} /></td>
                     </tr>
                     <tr>
                     <th scope="row">Total impuestos mensual</th>
-                    <td><input  type="number" name="total" id="" value={taxes.total} onChange={handleChangeTaxes} /></td>
+                    <td><input disabled type="number" name="taxes_total" id="" value={report.taxes_total} onChange={handleChange} /></td>
                     </tr>
                 </tbody>
                 </table><br />
